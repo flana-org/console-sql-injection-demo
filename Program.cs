aac9c8113bac41a309e14f3569818070ef54b0ba
0GiS0 <giselatb@outlook.com>
2025-10-21 10:19:48 +0200

    Add initial project files including devcontainer, .gitignore, and Program.cs

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Data.SqlClient;
+
+Console.WriteLine("Hello, World!");
+// user id
+string userId = Console.ReadLine();
+
+Console.WriteLine($"User ID is: {userId}");
+
+string connectionString = "YourConnectionString";
+string query = "SELECT * FROM Users WHERE UserId = '" + userId + "'";
+using (SqlConnection connection = new SqlConnection(connectionString))
+{
+    SqlCommand command = new SqlCommand(query, connection);
+    connection.Open();
+    SqlDataReader reader = command.ExecuteReader();
+    while (reader.Read())
+    {
+        Console.WriteLine($"{reader["UserName"]}, {reader["Email"]}");
+    }
+}
